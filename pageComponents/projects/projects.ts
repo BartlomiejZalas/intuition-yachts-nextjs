e2993b2db5e2b22bd130fb3b6ed1a6d9a6f73fbb
Bartlomiej Zalas <bartlomiej.zalas@nokia.com>
2020-06-04 16:36:04 +0200

    3 in row images per project

--- pageComponents/projects/projects.ts
+++ pageComponents/projects/projects.ts
@@ -5,9 +5,9 @@ export interface Project {
 }
 
 export const PROJECTS: Project[] = [
-  { id: 'iboat-6', title: 'iBoat-6', images: 8 },
-  { id: 'iboat-7', title: 'iBoat-7', images: 5 },
-  { id: 'it-800', title: 'iT-800', images: 8 },
+  { id: 'iboat-6', title: 'iBoat-6', images: 9 },
+  { id: 'iboat-7', title: 'iBoat-7', images: 6 },
+  { id: 'it-800', title: 'iT-800', images: 9 },
   { id: 'itender-8', title: 'iTender-8', images: 12 },
   { id: 'canna', title: 'Canna', images: 12 },
   { id: 'eco-14', title: 'ECO-14', images: 12 },
